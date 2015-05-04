53b907cc030e6a98a5816d81135073af5d62baf2
Geoffroy Lesage <gefthefrench@gmail.com>
2015-05-04 11:31:55 -0500

    Added intelij project & functionalized server start

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,12 @@
 import Main.Network.ListenSocket;
 
 public class Server {
-    private static final int port = 5555;
+    public static void main(String[] args) {
+        startServer(5555);
+    }
 
-    public static void main(String[] args){
+    private static void startServer(int port) {
+        System.out.print("Started server on port: " + String.valueOf(port));
         ListenSocket listen = new ListenSocket(port);
     }
 }
